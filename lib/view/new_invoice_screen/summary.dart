56afe3d793015ac00041930586c190606cf1d189
SohamDesai14 <soham.desai@hexafoldtech.com>
2025-02-24 15:42:14 +0530

    feat: :sparkles: refactored new invoice and new item section

--- lib/view/new_invoice_screen/summary.dart
+++ lib/view/new_invoice_screen/summary.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:invoice_maker/core/constants/app_colors.dart';
-import 'package:invoice_maker/core/constants/app_sizes.dart';
-import 'package:invoice_maker/core/constants/app_strings.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../core/utils/app_text_styles.dart';
+import "../../core/constants/app_colors.dart";
+import '../../core/constants/app_fonts_styles.dart';
+import '../../core/constants/app_sizes.dart';
+import '../../core/constants/app_strings.dart';
 
 class Summary extends StatelessWidget {
   final double amount;
@@ -15,37 +18,47 @@ class Summary extends StatelessWidget {
         const Text(AppStrings.summaryText),
         const SizedBox(height: AppSizes.s4),
         Container(
-          decoration: BoxDecoration(
-              border: Border.all(color: AppColors.grey),
-              borderRadius: BorderRadius.circular(8),
-              color: AppColors.white),
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Text(
+                Text(
                   AppStrings.totalCapitalText,
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                  style: AppTextStyles.helveticaNeue(
+                      AppColors.black, FontWeightStyles.bold, AppSizes.s17.r),
                 ),
                 Row(
                   children: [
-                    DropdownButton<String>(
-                      value: 'INR',
-                      items: ['INR', 'USD', 'EUR']
-                          .map((currency) => DropdownMenuItem<String>(
-                                value: currency,
-                                child: Text(currency),
-                              ))
-                          .toList(),
-                      onChanged: (value) {
-                        // Handle currency change
-                      },
+                    Container(
+                      height: AppSizes.s30.r,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, width: 1.5),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton<String>(
+                          value: 'INR',
+                          items: ['INR', 'USD', 'EUR']
+                              .map((currency) => DropdownMenuItem<String>(
+                                    value: currency,
+                                    child: Text(currency),
+                                  ))
+                              .toList(),
+                          onChanged: (value) {
+                            // Handle currency change
+                          },
+                        ),
+                      ),
                     ),
                     const SizedBox(width: AppSizes.s8),
                     Text(
                       '₹ $amount',
-                      style: const TextStyle(fontWeight: FontWeight.bold),
+                      style: AppTextStyles.helveticaNeue(AppColors.black,
+                          FontWeightStyles.bold, AppSizes.s17.r),
                     ),
                   ],
                 ),
