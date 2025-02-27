42c78f20039e38de0b90129de69ed237693b44a4
SohamDesai14 <soham.desai@hexafoldtech.com>
2025-02-27 10:32:02 +0530

    refactor: :art: changed and refactored for readability

---
+++ lib/core/utils/add_new_client.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../providers/client_provider.dart';
+import '../../view/new_invoice_screen/new_client_screen.dart';
+import '../../core/utils/app_text_styles.dart';
+import '../../core/utils/bottom_sheet.dart';
+import '../../core/constants/app_colors.dart';
+import '../../core/constants/app_fonts_styles.dart';
+import '../../core/constants/app_sizes.dart';
+import '../../core/constants/app_strings.dart';
+
+Widget buildAddClientButton(BuildContext ctx, Function(BuildContext) onTap) {
+  return Padding(
+    padding: EdgeInsets.only(left: AppSizes.s16.r, bottom: AppSizes.s7.r),
+    child: GestureDetector(
+      child: Row(
+        children: [
+          const Icon(
+            Icons.add,
+            color: AppColors.green,
+            size: AppSizes.s20,
+          ),
+          const SizedBox(width: AppSizes.s8),
+          GestureDetector(
+            onTap: () {
+              Provider.of<ClientProvider>(ctx, listen: false).clearForm();
+              CustomBottomSheet(
+                type: CustomBottomSheetType.fixed,
+                header: _buildHeader(ctx, onTap),
+                fixedheightFactor: 0.88,
+                mainContent: const Expanded(
+                  child: NewClientScreen(),
+                ),
+              ).showCustomBottomSheet();
+            },
+            child: Text(
+              AppStrings.addNewClientText,
+              style: AppTextStyles.helveticaNeueItem(
+                  AppColors.green, FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget _buildHeader(BuildContext ctx, Function(BuildContext) onTap) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      GestureDetector(
+        onTap: () {
+          Provider.of<ClientProvider>(ctx, listen: false).clearForm();
+          Navigator.pop(ctx);
+        },
+        child: Padding(
+          padding: EdgeInsets.only(left: AppSizes.s12.r),
+          child: Text(
+            AppStrings.cancelText,
+            style: AppTextStyles.helveticaNeue(
+                AppColors.black, FontWeightStyles.regular, AppSizes.s17.r),
+          ),
+        ),
+      ),
+      GestureDetector(
+        onTap: () {
+          onTap;
+        },
+        child: Padding(
+          padding: EdgeInsets.only(right: AppSizes.s12.r),
+          child: Text(
+            AppStrings.doneText,
+            style: AppTextStyles.helveticaNeue(
+                AppColors.black, FontWeightStyles.regular, AppSizes.s17.r),
+          ),
+        ),
+      ),
+    ],
+  );
+}
--- lib/view/new_invoice_screen/client_screen.dart
+++ lib/view/new_invoice_screen/client_screen.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
+import '../../core/utils/add_new_client.dart';
 import '../../core/utils/app_text_styles.dart';
-import '../../core/utils/bottom_sheet.dart';
 import '../../core/constants/app_colors.dart';
 import '../../core/constants/app_sizes.dart';
 import '../../core/constants/app_strings.dart';
-import '../../core/constants/app_fonts_styles.dart';
 import '../../core/constants/global_key.dart';
 import '../../providers/client_provider.dart';
 import '../../providers/form_provider.dart';
-import "new_client_screen.dart";
 
 class ClientScreen extends StatefulWidget {
   const ClientScreen({super.key});
@@ -65,41 +63,7 @@ class _ClientScreenState extends State<ClientScreen> {
                 const SizedBox(
                   height: AppSizes.s14,
                 ),
-                Padding(
-                  padding: EdgeInsets.only(
-                      left: AppSizes.s16.r, bottom: AppSizes.s7.r),
-                  child: GestureDetector(
-                    child: Row(
-                      children: [
-                        const Icon(
-                          Icons.add,
-                          color: AppColors.green,
-                          size: AppSizes.s20,
-                        ),
-                        const SizedBox(width: AppSizes.s8),
-                        GestureDetector(
-                          onTap: () {
-                            Provider.of<ClientProvider>(context, listen: false)
-                                .clearForm();
-                            CustomBottomSheet(
-                              type: CustomBottomSheetType.fixed,
-                              header: _buildHeader(ctx),
-                              fixedheightFactor: 0.88,
-                              mainContent: const Expanded(
-                                child: NewClientScreen(),
-                              ),
-                            ).showCustomBottomSheet();
-                          },
-                          child: Text(
-                            AppStrings.addNewClientText,
-                            style: AppTextStyles.helveticaNeueItem(
-                                AppColors.green, FontWeight.bold),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                buildAddClientButton(ctx, onSaveClient),
                 Expanded(
                   child: Padding(
                     padding: EdgeInsets.only(left: AppSizes.s6.r),
@@ -198,40 +162,5 @@ class _ClientScreenState extends State<ClientScreen> {
         clientProvider.clearForm();
       }
     }
-  }
-
-  Widget _buildHeader(BuildContext ctx) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        GestureDetector(
-          onTap: () {
-            Provider.of<ClientProvider>(context, listen: false).clearForm();
-            Navigator.pop(ctx);
-          },
-          child: Padding(
-            padding: EdgeInsets.only(left: AppSizes.s12.r),
-            child: Text(
-              AppStrings.cancelText,
-              style: AppTextStyles.helveticaNeue(
-                  AppColors.black, FontWeightStyles.regular, AppSizes.s17.r),
-            ),
-          ),
-        ),
-        GestureDetector(
-          onTap: () {
-            onSaveClient(ctx);
-          },
-          child: Padding(
-            padding: EdgeInsets.only(right: AppSizes.s12.r),
-            child: Text(
-              AppStrings.doneText,
-              style: AppTextStyles.helveticaNeue(
-                  AppColors.black, FontWeightStyles.regular, AppSizes.s17.r),
-            ),
-          ),
-        ),
-      ],
-    );
   }
 }
