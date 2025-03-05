37d87e56b0492cb6a1c86928e33aa50a0fdc07e0
SohamDesai14 <soham.desai@hexafoldtech.com>
2025-03-05 13:18:24 +0530

    fix(discount): :ambulance: storing & displaying discount correctly

--- lib/view/widgets/item_display.dart
+++ lib/view/widgets/item_display.dart
@@ -69,7 +69,7 @@ class ItemDisplay extends StatelessWidget {
                 style: AppTextStyles.helveticaNeue(
                     AppColors.grey, FontWeightStyles.regular, AppSizes.s15.r),
               ),
-              discount!.isNotEmpty && discount!.contains('0.0')
+              !discount!.contains('0.0')
                   ? TextSpan(
                       text: " - $discount ${AppStrings.percentSymbolText} DISC",
                       style: AppTextStyles.helveticaNeue(AppColors.grey,
